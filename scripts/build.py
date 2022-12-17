0dd4988514ebad7ffda3b04175356019317fd7eb
Marc <marcnew2101@gmail.com>
2022-12-17 08:45:58 -0700

    Added manifest values and debug options

--- scripts/build.py
+++ scripts/build.py
@@ -1,10 +1,17 @@
 import sys
 import json
+from pathlib import Path
 
+# define root directory (/roku-boilerplate)
+rootDir = Path(__file__).parents[1]
+# define file paths for config and manifest
+configFile = rootDir / 'components' / 'data' / 'config.json'
+manifestFile = rootDir / 'manifest'
+# get build from script args
 build = sys.argv[1:][0]
+# define default log levels
 debugLogLevel = 0
-configFile = './components/data/config.json'
-manifestFile = './manifest'
+logging = -1
 
 buildTypes = [
     'release',
@@ -19,10 +26,17 @@ if build in buildTypes:
                 sys.exit('config.json is missing key "buildType"')
             else:
                 data['buildType'] = build
-            if 'debugLogLevel' not in data:
-                sys.exit('config.json is missing key "debugLogLevel"')
-            else:
-                debugLogLevel = data['debugLogLevel']
+                if build == 'debug':
+                    try:
+                        logging = int(input('Specify debug logging level? (0, 1, 2, 3, 4) \nor press ENTER to use previously set log level in config: '))
+                    except:
+                        pass
+                    if 'debugLogLevel' not in data:
+                        sys.exit('config.json is missing key "debugLogLevel"')
+                    else:
+                        debugLogLevel = logging if logging >= 0 else data['debugLogLevel']
+                        data['debugLogLevel'] = debugLogLevel
+                        print("logging level set to " + str(debugLogLevel))
             if 'manifest' not in data:
                 sys.exit('config.json is missing key "manifest"')
             else:
@@ -30,12 +44,15 @@ if build in buildTypes:
                     with open(manifestFile, 'w') as manifest:
                         manifestItems = data['manifest']
                         if len(manifestItems) > 0:
-                            for key, value in manifestItems.items():
-                                manifest.write(key + '=' + str(value) + '\n')
-                        if build == 'debug':
-                            manifest.write('debug=true\n')
-                        else:
-                            manifest.write('debug=false\n')
+                            for item in manifestItems:
+                                manifest.write(item['comment'] + '\n')
+                                for key, value in item.items():
+                                    if key != 'note' and key != 'comment':
+                                        manifest.write(key + '=' + str(value) + '\n')
+                                manifest.write('\n')
+                        manifest.write('## Debug Settings ##' + '\n')
+                        isDebug = 'true' if build == 'debug' else 'false'
+                        manifest.write('debug=' + isDebug + '\n')
                         manifest.write('debug_log_level=' + str(debugLogLevel) + '\n')
                 except FileNotFoundError:
                         print(f'{manifestFile} not found')
